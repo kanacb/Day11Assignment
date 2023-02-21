9777e1ac645912b86f47da992abfa373b35444ee
Kana Sabaratnam <kana@cloudbasha.com>
2023-02-21 14:55:56 +0800

    Feb 21

--- websocket-client/src/main/java/com/rabbit/samples/springwebsocketclient/rest/WebSocketClientRestController.java
+++ websocket-client/src/main/java/com/rabbit/samples/springwebsocketclient/rest/WebSocketClientRestController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PreDestroy;
-import java.rmi.activation.ActivationGroup_Stub;
+// import java.rmi.activation.ActivationGroup_Stub;
 
 
 /**
